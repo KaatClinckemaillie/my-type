66bc1a6ef54f66c1bbfd686069e57df3f1ec1b80
KaatClinckemaillie <kaat.clinckemaillie@student.howest.be>
2022-01-17 18:46:01 +0100

    change model #45

--- src/model/Letter.php
+++ src/model/Letter.php
@@ -2,7 +2,7 @@
 
 use \Illuminate\Database\Eloquent\Model;
 
-class Demo extends Model {
+class Letter extends Model {
   // refer to a database table, an object us used here for demo purposes
   public $timestamps = false;
 
--- src/view/pages/index.php
+++ src/view/pages/index.php
@@ -1,9 +1,5 @@
 <section class="content">
 <ul class="demo-list">
-<?php
-    foreach ($demos as $demo){
-      echo('<li>' . $demo->name . '</li>');
-    }
-  ?>
+  <li>hello</li>
   </ul>
 </section>
