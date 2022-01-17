39208380eb82533ad38152b1571632436c5988cc
KaatClinckemaillie <kaat.clinckemaillie@student.howest.be>
2022-01-17 20:11:30 +0100

    place menu link #42

--- src/view/layout.php
+++ src/view/layout.php
@@ -5,11 +5,14 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php echo $css; ?>
-  <title>Webpack - php - template</title>
+  <title>Bembo Big Time</title>
 </head>
 <body>
   <div class="container">
-      <header><h1 class="pagetitle">Webpack - PHP - Template</h1></header>
+      <header class="menu">
+        <h1 class="pagetitle hidden">Bembo Big Time</h1>
+        <a href="" class="menu__link">MENU</a>
+      </header>
       <?php echo $content;?>
   </div>
   <?php echo $js; ?>
