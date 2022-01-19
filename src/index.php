57b01d28277a2186dcbea8db655631a88c1ebd73
KaatClinckemaillie <kaat.clinckemaillie@student.howest.be>
2022-01-19 12:35:47 +0100

    solve notification #45

--- src/index.php
+++ src/index.php
@@ -1,8 +1,14 @@
 <?php
-session_start();
-ini_set('display_errors', true);
-error_reporting(E_ALL);
-
+if (basename(dirname(dirname(__FILE__))) != 'src') {
+  // do not show warnings or errors in production mode
+  session_start();
+  ini_set('display_errors', false);
+}else{
+  // show everything in development mode
+  session_start();
+  ini_set('display_errors', true);
+  error_reporting(E_ALL);
+}
 // parsing the .env file if available
 // convert content to key / value pairs
 if (file_exists("../.env")) {
