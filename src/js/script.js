de9fd3adb5ab4861fee5ea747b023909957d807c
KaatClinckemaillie <kaat.clinckemaillie@student.howest.be>
2022-01-20 17:55:07 +0100

    load letters with js #46

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,6 @@
 const $canvasHeader = document.querySelector('.header__canvas');
 const ctx = $canvasHeader.getContext('2d');
 
-const letters = [];
-
 const animate = () => {
   ctx.clearRect(0, 0, $canvasHeader.width, $canvasHeader.height);
   letters.forEach(letter => letter.draw());
@@ -10,8 +8,16 @@ const animate = () => {
   requestAnimationFrame(animate);
 };
 
+const getLetters = async () => {
+  console.log('Start loading the JSON file');
+  const response = await fetch(`index.php?page=api-letters`);
+  const letters = await response.json();
+  console.log(letters);
+  //animate();
+  return letters;
+};
+
 export const init = () => {
   console.log('start executing this JavaScript');
-
-  animate();
+  getLetters();
 };
